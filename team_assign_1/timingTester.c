86f6fe80a5992dce1f07bf8c71ddbe960571d608
Brent Mellor <BrentMellor@Gmail.com>
2014-11-03 14:28:45 -0700

    Added more global populate code.

--- team_assign_1/timingTester.c
+++ team_assign_1/timingTester.c
@@ -35,14 +35,18 @@ int main()
         clock_gettime(CLOCK_MONOTONIC, &stop);
         timingData[i] = diff(start,stop);
         fprintf(rawData,"%d %ld\n",i,timingData[i].tv_nsec);
-//        timingData[i].tv_nsec = (timingData[i-1].tv_nsec*59+439)%7919;  // Random data for testing
     }    
     fclose(rawData);
     printf("Done running timing test.\n");
     generate_ECDF(timingData, NUM_TESTS, PLOT_POINTS);   
     free(timingData);
     
-	return 0;
+    printf("CMV = [%d,%d,%d,%d,%d,%d,%d,%d,%d,%d,%d,%d,%d,%d,%d]\n",CMV[0],CMV[1],CMV[2],CMV[3],CMV[4],CMV[5],CMV[6],CMV[7],CMV[8],CMV[9],CMV[10],CMV[11],CMV[12],CMV[13],CMV[14]);
+    printf("FUV = [%d,%d,%d,%d,%d,%d,%d,%d,%d,%d,%d,%d,%d,%d,%d]\n",FUV[0],FUV[1],FUV[2],FUV[3],FUV[4],FUV[5],FUV[6],FUV[7],FUV[8],FUV[9],FUV[10],FUV[11],FUV[12],FUV[13],FUV[14]);
+
+    printf("Launch Missile: %s\n",(LAUNCH)? "YES": "NO");
+
+    return 0;
 }
 
 
@@ -72,7 +76,6 @@ void generate_ECDF(struct timespec* dataSet, unsigned int dataSet_size, unsigned
        if(dataSet[i].tv_nsec > max) 
             max = dataSet[i].tv_nsec;
     }
-    printf("max value: %ld",max); 
     FILE *plotData = fopen("plotData", "w+");
     
     // generate ECDF
@@ -91,25 +94,20 @@ void generate_ECDF(struct timespec* dataSet, unsigned int dataSet_size, unsigned
         //printf("%ld %f\n",x_coord,y_coord);
     }
     fclose(plotData);
-    /*
-    FILE *pipe = popen("gnuplot -persist", "w");
-    fprintf(pipe, "set title 'Empirical Cumulative Distribution Function Plot' font 'sans,18';");
-    fprintf(pipe, "set xlabel 'Time (nS)' font 'sans,14';");
-    fprintf(pipe, "set ylabel 'ECFD \%%' font 'sans,14';");
-    fprintf(pipe, "set grid; unset key;");
-    fprintf(pipe, "plot 'plotData' with lines;");
+    
+    FILE *pipe = popen("gnuplot ", "w");
+    fprintf(pipe, "plot sin(x)");
+  // fprintf(pipe, "set title 'Empirical Cumulative Distribution Function Plot' font 'sans,18';");
+   // fprintf(pipe, "set xlabel 'Time (nS)' font 'sans,14';");
+   // fprintf(pipe, "set ylabel 'ECFD \%%' font 'sans,14';");
+   // fprintf(pipe, "set grid; unset key;");
+   // fprintf(pipe, "plot 'plotData' with lines;");
     pclose(pipe);
-    */
+    
     
 }
 
 void populateGlobals() {
-
-    int i;
-    for(i=0; i < 100; i++) {
-        X[i] = ((i)*59+439)%7919;  // Random data for testing
-        Y[i] = ((i)*79+691)%8017;  // Random data for testing
-    }    
 
     PARAMETERS.LENGTH1 = 11;   // Length in LICs 0, 7, 12
     PARAMETERS.RADIUS1 = 5;   // Radius in LICs 1, 8, 13
@@ -130,6 +128,59 @@ void populateGlobals() {
     PARAMETERS.LENGTH2 = 31;   // Maximum length in LIC 12
     PARAMETERS.RADIUS2 = 23;   // Maximum radius in LIC 13
     PARAMETERS.AREA2 = 41;     // Maximum area in LIC 14
+
+      double X_init[100] = {1,3,5,7,-2,-2,-2,-2,0,6,1,1,5,0,4.11,16.13,0,1,2,1,1,-1,1,-1,1,-1,1,1,0,0,3,3,-1,};
+      double Y_init[100] = {2,4,6,8,-2,0,6,0,0,0,1,1,1,0,-7.37,-9.35,0,2,0,-4,2,-1,-4,-1,2,-1,-4,2,1,15,3,-1,-1,}; 
+
+    CONNECTORS LCM_init[15][15] = {
+        {ORR,ANDD,NOTUSED,ORR,NOTUSED,ANDD,NOTUSED,ORR,ANDD,ANDD,ORR,NOTUSED,ANDD,ORR,NOTUSED},
+        {ANDD,ORR,NOTUSED,ANDD,NOTUSED,ORR,ANDD,ANDD,ORR,NOTUSED,ANDD,NOTUSED,NOTUSED,ORR,NOTUSED},
+        {NOTUSED,ANDD,ANDD,ORR,ORR,NOTUSED,ORR,NOTUSED,ANDD,ORR,NOTUSED,ANDD,NOTUSED,ORR,NOTUSED},
+        {ANDD,ORR,NOTUSED,ANDD,NOTUSED,ORR,ANDD,ANDD,ORR,NOTUSED,ANDD,NOTUSED,NOTUSED,ORR,NOTUSED},
+        {NOTUSED,ANDD,ANDD,ORR,ORR,NOTUSED,ORR,NOTUSED,ANDD,ORR,NOTUSED,ANDD,NOTUSED,ORR,NOTUSED},
+        {ORR,ANDD,NOTUSED,ORR,NOTUSED,ANDD,NOTUSED,ORR,ANDD,ANDD,ORR,NOTUSED,ANDD,ORR,NOTUSED},
+        {ANDD,ORR,NOTUSED,ANDD,NOTUSED,ORR,ANDD,ANDD,ORR,NOTUSED,ANDD,NOTUSED,NOTUSED,ORR,NOTUSED},
+        {NOTUSED,ANDD,ANDD,ORR,ORR,NOTUSED,ORR,NOTUSED,ANDD,ORR,NOTUSED,ANDD,NOTUSED,ORR,NOTUSED},
+        {ORR,ANDD,NOTUSED,ORR,NOTUSED,ANDD,NOTUSED,ORR,ANDD,ANDD,ORR,NOTUSED,ANDD,ORR,NOTUSED},
+        {ANDD,ORR,NOTUSED,ANDD,NOTUSED,ORR,ANDD,ANDD,ORR,NOTUSED,ANDD,NOTUSED,NOTUSED,ORR,NOTUSED},
+        {NOTUSED,ANDD,ANDD,ORR,ORR,NOTUSED,ORR,NOTUSED,ANDD,ORR,NOTUSED,ANDD,NOTUSED,ORR,NOTUSED},
+        {ANDD,ORR,NOTUSED,ANDD,NOTUSED,ORR,ANDD,ANDD,ORR,NOTUSED,ANDD,NOTUSED,NOTUSED,ORR,NOTUSED},
+        {NOTUSED,ANDD,ANDD,ORR,ORR,NOTUSED,ORR,NOTUSED,ANDD,ORR,NOTUSED,ANDD,NOTUSED,ORR,NOTUSED},
+        {ORR,ANDD,NOTUSED,ORR,NOTUSED,ANDD,NOTUSED,ORR,ANDD,ANDD,ORR,NOTUSED,ANDD,ORR,NOTUSED},
+        {ANDD,ORR,NOTUSED,ANDD,NOTUSED,ORR,ANDD,ANDD,ORR,NOTUSED,ANDD,NOTUSED,NOTUSED,ORR,NOTUSED}};
+    
+    boolean PUM_init[15][15] = {
+        {1,0,0,0,0,0,0,0,0,0,0,0,0,0,0},
+        {0,1,0,0,0,0,0,0,0,0,0,0,0,0,0},
+        {0,0,1,0,0,0,0,0,0,0,0,0,0,0,0},
+        {0,0,0,1,0,0,0,0,0,0,0,0,0,0,0},
+        {0,0,0,0,1,0,0,0,0,0,0,0,0,0,0},
+        {0,0,0,0,0,1,0,0,0,0,0,0,0,0,0},
+        {0,0,0,0,0,0,1,0,0,0,0,0,0,0,0},
+        {0,0,0,0,0,0,0,1,0,0,0,0,0,0,0},
+        {0,0,0,0,0,0,0,0,1,0,0,0,0,0,0},
+        {0,0,0,0,0,0,0,0,0,1,0,0,0,0,0},
+        {0,0,0,0,0,0,0,0,0,0,1,0,0,0,0},
+        {0,0,0,0,0,0,0,0,0,0,0,1,0,0,0},
+        {0,0,0,0,0,0,0,0,0,0,0,0,1,0,0},
+        {0,0,0,0,0,0,0,0,0,0,0,0,0,1,0},
+        {0,0,0,0,0,0,0,0,0,0,0,0,0,0,1}};
+
+
+    int i, j;
+    for(i=0; i < 15; i++) {
+        CMV[i] = 0;
+        FUV[i] = 0;
+        for(j=0; j < 15; j++) {
+            LCM[i][j] = LCM_init[i][j];
+            PUM[i][j] = PUM_init[i][j];
+        }
+    }
+
+    for(i=0; i < 100; i++) {
+        X[i] = X_init[i];
+        Y[i] = Y_init[i];
+    }
 }
 
 void fail(char* msg) {
